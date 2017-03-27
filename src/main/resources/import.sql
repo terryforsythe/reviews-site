c1a741012a043e3891caed14dea85cb3db5ec655
terrythompson <b.terry.thompson@gmail.com>
2017-03-27 11:17:19 -0400

    gradle build test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,6 +6,9 @@ INSERT INTO Category(id, title) VALUES(4, 'Animated')
 
 // initialize reviews
 INSERT INTO Review(title, image_Url, category_id, content) VALUES('Road Warrior, The', 'imageUrl', 1, 'content')
-INSERT INTO Review(title, image_Url, category_id, content) VALUES('Get Out', 'imageUrl', 3, 'content')
+INSERT INTO Review(title, image_Url, category_id, content) VALUES('Dude, Wheres My Car?', 'imageUrl', 2, 'content')
+INSERT INTO Review(title, image_Url, category_id, content) VALUES('Dawn of the Dead', 'imageUrl', 3, 'content')
+INSERT INTO Review(title, image_Url, category_id, content) VALUES('Nausicaa and the Valley of the Wind', 'imageUrl', 4, 'content')
+
 
 
